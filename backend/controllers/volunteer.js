d55ac13c34048622f2ddc2aa5d8c9bd6a259e4ff
CreditableX <69977356+CreditableX@users.noreply.github.com>
2024-01-31 20:00:08 +0800

    Backend work on user/admin functions

--- backend/controllers/volunteer.js
+++ backend/controllers/volunteer.js
@@ -1,11 +1,30 @@
 // handles api requests for all volunteer tasks
 
 export async function getVolunteers(req, res) {
+    try {
+        const getVolunteersQuery = {
+            text: "SELECT * FROM users",
+        }
 
+        return await pool.query(getVolunteersQuery); //returns a promise
+    } catch (error) {
+        console.error("Error querying SQL: ", error);
+        throw error;
+    }
 }
 
 export async function getSingleVolunteer(req, res) {
+    try {
+        const getVolunteerQuery = {
+            text: "SELECT * FROM users WHERE username = $1",
+            values: [username]
+        }
 
+        return await pool.query(getVolunteerQuery); //returns a promise
+    } catch (error) {
+        console.error("Error querying SQL: ", error);
+        throw error;
+    }
 }
 
 export async function getCertificate(req, res) {
