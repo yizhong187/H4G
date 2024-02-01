c119e02c097c3c2ef4e8454246e7aebf227eb0e6
yizhong187 <yizhong187@gmail.com>
2024-02-01 13:43:30 +0800

    trying out container; toolbar necessary to stop it from clipping in the top

--- frontend/src/pages/CertificatesPage.tsx
+++ frontend/src/pages/CertificatesPage.tsx
@@ -1,6 +1,31 @@
+import {
+  Box,
+  Container,
+  Grid,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 const CertificatesPage: React.FC = () => {
-
-  return <div>This is the Certificates page</div>
-}
+  return (
+    <Box
+      component="main"
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[100]
+            : theme.palette.grey[900],
+        flexGrow: 1,
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <Toolbar />
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Typography variant="h1">This is Certifcates Page!</Typography>
+      </Container>
+    </Box>
+  );
+};
 
 export default CertificatesPage;
