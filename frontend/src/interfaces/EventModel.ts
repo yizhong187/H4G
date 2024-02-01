26a4791e343130b4d6e5240d9b2a9980d52b8fdd
yizhong187 <yizhong187@gmail.com>
2024-02-02 00:47:07 +0800

    added personInCharge prop to eventmodel

--- frontend/src/interfaces/EventModel.ts
+++ frontend/src/interfaces/EventModel.ts
@@ -2,6 +2,7 @@ export interface EventModel {
   id: string;
   title: string;
   description: string;
+  personInCharge: string;
   date: string;
   location: string;
   quota: number;
@@ -9,10 +10,11 @@ export interface EventModel {
   status: 'open' | 'closed'; 
 }
 
-const sampleEvent: EventModel = {
+export const sampleEvent: EventModel = {
   id: "e456",
   title: "Beach Cleanup",
-  description: "A community event to clean the local beach.",
+  description: "Phasellus rhoncus turpis nisl, ac aliquam elit venenatis ac. Maecenas elementum pretium libero, in pharetra nisl pharetra quis. Nunc placerat purus eu hendrerit rutrum. Cras dignissim urna ut neque sodales condimentum. Nulla feugiat volutpat ex eget consectetur. Suspendisse mattis tortor sit amet massa congue, eget vehicula ipsum pretium. In hac habitasse platea dictumst. Ut nec fermentum odio. Maecenas id rutrum massa, eget rutrum quam. Proin at sapien sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque fermentum quis leo non pulvinar. Suspendisse et luctus neque. Phasellus ullamcorper a ipsum vitae vestibulum. Aliquam erat volutpat. Proin vitae enim nisi.",
+  personInCharge: "Mr Lee Chong Tok",
   date: "2024-02-20",
   location: "West Coast Park",
   quota: 50,
