b30f2b16d6f96cb006be47e4ec761569810babec
yizhong187 <yizhong187@gmail.com>
2024-02-01 13:43:40 +0800

    edited homepage with basic containter

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,55 @@
+import { Box, Container, Grid, Paper, Toolbar } from "@mui/material";
+
 const HomePage: React.FC = () => {
+  console.log("Rendered!");
 
-  console.log("Rendered!")
-
-  return <div>This is the homepage</div>
-}
+  return (
+    <Box
+      component="main"
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[100]
+            : theme.palette.grey[900],
+        flexGrow: 1,
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <Toolbar />
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                height: 240,
+              }}
+            ></Paper>
+          </Grid>
+          {/* Recent Deposits */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                height: 240,
+              }}
+            ></Paper>
+          </Grid>
+          {/* Recent Orders */}
+          <Grid item xs={12}>
+            <Paper
+              sx={{ p: 2, display: "flex", flexDirection: "column" }}
+            ></Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
 
 export default HomePage;
