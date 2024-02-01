96349930f816184eac227ab0d4ada55d35c6f62d
yizhong187 <yizhong187@gmail.com>
2024-02-01 10:02:58 +0800

    removed index.css and App.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,36 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./routes/routes";
 import { UserProvider } from "./contexts/UserContext";
-import { UserNavbar } from "./components/UserNavbar/UserNavbar";
-import { MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
-import "./App.css";
+
+import UserNavbar from "./components/UserNavbar";
 
 const App: React.FC = () => {
   return (
-    <MantineProvider>
-      <UserProvider>
+    <UserProvider>
       <Router>
-      <div className="appContainer">
-            <UserNavbar />
-          </div>
-          <div className="mainContent">
-            <Routes>
-              {routes.map((route, index) => {
-                const Component = route.component;
-                return (
-                  <Route
-                    key={index}
-                    path={route.path}
-                    element={<Component />}
-                  />
-                );
-              })}
-            </Routes>
-          </div>
-        </Router>
-      </UserProvider>
-    </MantineProvider>
+        <UserNavbar />
+        <Routes>
+          {routes.map((route, index) => {
+            const Component = route.component;
+            return (
+              <Route key={index} path={route.path} element={<Component />} />
+            );
+          })}
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 };
 
