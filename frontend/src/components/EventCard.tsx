4016d105eaab95500ad346b4b7d0b90c2c4d6857
kennylewi <kennylewi27@gmail.com>
2024-02-02 17:39:50 +0800

    git testing

--- frontend/src/components/EventCard.tsx
+++ frontend/src/components/EventCard.tsx
@@ -5,7 +5,7 @@ import moment from "moment";
 import { useNavigate } from "react-router-dom";
 import { EventModel } from "../interfaces/EventModel";
 
-//this is a new edit to test git 2
+//this is a new edit to test git 3
 
 type EventCardProps = {
   event: EventModel;
