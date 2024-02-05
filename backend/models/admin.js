7e1d720c4c019bf191827f9c576096c4335a3f8a
Casper Ng <casperngeen@gmail.com>
2024-02-05 16:38:07 +0800

    Update router documentation

--- backend/models/admin.js
+++ backend/models/admin.js
@@ -7,7 +7,7 @@ export async function insertAdmin(username, hash) {
             text: "INSERT INTO users(username, password_hash, adminRights) VALUES($1, $2)",
             values: [username, hash, true] 
         }
-        return await pool.query(insertAdminQuery); //returns a promise
+        return await pool.query(insertAdminQuery);
     } catch (error) {
         console.error("Error querying SQL: ", error);
         throw error;
@@ -21,7 +21,40 @@ export async function findUserEvents(username) {
             text: "SELECT * FROM registeredEvents WHERE username = $1",
             values: [username]
         }
-        return await pool.query(findUserQuery); //returns a promise
+        return await pool.query(findUserQuery);
+    } catch (error) {
+        console.error("Error querying SQL: ", error);
+        throw error;
+    }
+}
+
+// to edit
+export async function getHoursByMonth(month, year) {
+    try {
+        const fetchHoursByMonth = {
+            text: `SELECT EXTRACT(MONTH FROM eventDate), SUM(hours), COUNT(DISTINCT eventID) 
+                    FROM volunteerHours
+                    WHERE `,
+            values: [month, year]
+        }
+        return await pool.query(fetchHoursByMonth);
+    } catch (error) {
+        console.error("Error querying SQL: ", error);
+        throw error;
+    }
+}
+
+// to edit
+export async function getGenderByMonth(year) {
+    try {
+        const fetchHoursByMonth = {
+            text: `SELECT EXTRACT(MONTH FROM eventDate), SUM(hours), COUNT(DISTINCT eventID),  
+                    FROM volunteerHours JOIN users
+                    ON userID = id
+                    WHERE `,
+            values: [year]
+        }
+        return await pool.query(fetchHoursByMonth);
     } catch (error) {
         console.error("Error querying SQL: ", error);
         throw error;
